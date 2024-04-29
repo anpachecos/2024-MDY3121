36e3abad16afd9112fa31318c9fa8a8eaceae3f9
anpachecos <an.pachecos@duocuc.cl>
2024-04-29 00:21:49 -0400

    Varray y con ayuda T.T

--- 4_EJERCICIO.sql
+++ 4_EJERCICIO.sql
@@ -1,37 +1,25 @@
-------CASO 4------
 DECLARE
     V_VENDEDOR  VENDEDOR%ROWTYPE;
-    V_MIN       NUMBER(4); 
-    V_MAX       NUMBER(4);
-    v_ID_VENDEDOR NUMBER(4):= 0;
+    TYPE tipo_varray_vendedor IS VARRAY(10) OF VENDEDOR.ID_VENDEDOR%TYPE;
+    varray_vendedor tipo_varray_vendedor := tipo_varray_vendedor();
 BEGIN
-    SELECT
-        MIN(ID_VENDEDOR),
-        MAX(ID_VENDEDOR)
-    INTO 
-        V_MIN, 
-        V_MAX
-    FROM VENDEDOR;
-    
-    FOR i in V_MIN..V_MAX LOOP
-        V_ID_VENDEDOR := i *5;
-        SELECT 
-         *
-        INTO
-            V_VENDEDOR
-        FROM
-            vendedor
-        WHERE
-            id_vendedor = v_id_vendedor;
-        
-        IF V_VENDEDOR.SUELDO <= 354000 THEN 
-            dbms_output.put_line( '['|| i ||']'|| ' Empleado N°' || V_VENDEDOR.id_vendedor ||
-            ' -' || V_VENDEDOR.nombres ||' ' || V_VENDEDOR.APELLIDOS ||' tiene un sueldo de ' ||
-            TO_CHAR(V_VENDEDOR.sueldo, '999G999G999'));  
+    FOR v_vendedor_rec IN (SELECT * FROM vendedor) LOOP
+        IF v_vendedor_rec.SUELDO <= 354000 THEN 
+            varray_vendedor.EXTEND;
+            varray_vendedor(varray_vendedor.LAST) := v_vendedor_rec.ID_VENDEDOR;
         END IF;
-    
     END LOOP;
+    
+    -- Imprimir el contenido de varray_vendedor con el formato solicitado
+    FOR i IN 1..varray_vendedor.COUNT LOOP
+        SELECT *
+        INTO V_VENDEDOR
+        FROM vendedor
+        WHERE ID_VENDEDOR = varray_vendedor(i);
         
+        DBMS_OUTPUT.PUT_LINE('[' || i || '] Empleado N°' || V_VENDEDOR.ID_VENDEDOR ||
+            ' - ' || V_VENDEDOR.NOMBRES || ' ' || V_VENDEDOR.APELLIDOS || ' tiene un sueldo de ' ||
+            TO_CHAR(V_VENDEDOR.SUELDO, '999G999G999'));
+    END LOOP;
 END;
-
-SELECT * FROM VENDEDOR;
+/
