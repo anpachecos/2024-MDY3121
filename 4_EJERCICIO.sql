198ac11405b0c757890025ceef24934e8922e891
anpachecos <an.pachecos@duocuc.cl>
2024-05-16 22:19:57 -0400

    Ejercicios Arreglados en Clase

--- 4_EJERCICIO.sql
+++ 4_EJERCICIO.sql
@@ -35,3 +35,46 @@ BEGIN
         END LOOP;
     CLOSE cur_vendedor;
 END;
+
+/*-------------Forma dos de hacer el ejercicio--------------*/
+DECLARE
+      
+
+    CURSOR cur_vendedor IS (
+        SELECT 
+            id_vendedor,
+            nombres, 
+            apellidos,
+            sueldo
+        FROM vendedor 
+            WHERE sueldo <= 354000);
+
+        --Esto nos crea una estructura la cual modemos usar las veces que queramos para almacenar los datos de un empleado
+        TYPE r_vendedor IS RECORD(
+            id_vendedor VENDEDOR.ID_VENDEDOR%TYPE,
+            nombres VENDEDOR.NOMBRES%TYPE,
+            apellidos VENDEDOR.APELLIDOS%TYPE,
+            sueldo VENDEDOR.SUELDO%TYPE
+        );
+        
+        v_vendedor r_vendedor; --De tipo Record 
+        
+BEGIN
+    OPEN cur_vendedor;
+    
+    DBMS_OUTPUT.PUT_LINE('--------------------------------------------------');
+    DBMS_OUTPUT.PUT_LINE('INFORME DE EMPLEADOS');
+    DBMS_OUTPUT.PUT_LINE('--------------------------------------------------');
+    
+    
+        LOOP
+            v_contador := v_contador + 1;
+            FETCH cur_vendedor INTO
+            v_vendedor;
+            EXIT WHEN cur_vendedor%NOTFOUND;
+            DBMS_OUTPUT.PUT_LINE( '['|| v_contador || '] ' || 'Empleado N° ' || v_vendedor.id_vendedor
+            || ' - ' || v_vendedor.nombres || ' ' || v_vendedor.apellidos || 
+            ' tiene un sueldo de ' || TO_CHAR(v_sueldo, '$999g999g999'));
+        END LOOP;
+    CLOSE cur_vendedor;
+END;
--- 5_EJERCICIO.sql
+++ 5_EJERCICIO.sql
@@ -51,3 +51,64 @@ BEGIN
     CLOSE cur_empleados;
     
 END;
+
+
+/*FORMA DEL PROFE
+
+Antonia Por favor repasa esto porque me simplificó la vida 
+Entiendelo por favorrrrrrrrrrrrrr. Aprende a usar Cursores y loops cursores loop cursores looooop*/
+
+DECLARE
+    CURSOS cur_departamentos IS (
+        SELECT 
+        id_categoria, 
+        nom_categoria
+         FROM categoria;
+    )
+
+    CURSOR cur_vendedores(categoria VARCHAR2) IS
+        SELECT 
+            v.id_vendedor,
+            v.nombres,
+            v.apellidos,
+            v.sueldo
+        FROM vendedor v
+        WHERE v.sueldo <= 400000
+        AND id_categoria = id_categoria
+        ORDER BY c.nom_categoria, v.id_vendedor;
+    TYPE r_vendedor IS RECORD(
+        id_vendedor vendedor.id_vendedor%TYPE,
+        nombres vendedor.nombres%TYPE,
+        apellidos vendedor.apellidos%TYPE,
+        sueldo vendedor.sueldo%TYPE
+    );
+
+        v_id_categoria         categoria.id_categoria%TYPE;
+        v_nom_categoria        categoria.nom_categoria%TYPE;
+BEGIN
+
+    LOOP
+        FETCH cur_categoria INTO
+            v_id_categoria,
+            v_nom_categoria;
+        
+            EXIT WHEN cur_categoria%NOTFOUND;
+
+            DBMS_OUTPUT.PUT_LINE('--------------------------------------------------');
+            DBMS_OUTPUT.PUT_LINE(v_nom_categoria || ':');
+
+            OPEN cur_vendores(v_id_categoria);
+            LOOP
+                FETCH cur_vendedores INTO
+                    v_vendedor;
+                EXIT WHEN cur_vendedores%NOTFOUND;
+                
+                DBMS_OUTPUT.PUT_LINE(
+                    RPAD(v_id_vendedor, 5) || 
+                    RPAD(v_nombres || ' ' || v_apellidos, 25) || 
+                    RPAD('$' || TO_CHAR(v_sueldo, '999,999'), 15));
+
+            END LOOP;
+            CLOSE cur_vendedores;
+            
+    END LOOP;
